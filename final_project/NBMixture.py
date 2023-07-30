923131ed6e15ecc72ce90ceb26f56b2aaee2c35c
Zleander <zimmermannleander@gmail.com>
2023-07-30 12:57:53 +0200

    alles mögliche

--- final_project/NBMixture.py
+++ final_project/NBMixture.py
@@ -2,43 +2,52 @@ import numpy as np
 
 class NBMixture():
 
-    def __init__(self, n_components: int = 1, n_S: int = 10, r=2):
+    def __init__(self, n_components: int = 1, n_S: int = None, maxiter: int = 20, r: float = 2):
         self.n_components = n_components
         self.n_S = n_S
+        self.maxiter = maxiter
+        self.r = r
         self.S = np.arange(n_S)
         self.pi = np.ones(n_components) / n_components
-        self.r = r
+
 
     def fit(self, X: np.ndarray):
         self.X = X
         self.n_cells, self.n_genes = self.X.shape
-        # TODO think about init of these
-        self.p = np.random.uniform(10, size=(self.n_genes, self.n_components))
-        self.p /= self.p.sum(axis=1, keepdims=True)
+        
+        self._run_em()
 
+    def _run_em(self):
+        
+        self.n_cells, self.n_genes = self.X.shape
+
+        # self.p = np.random.uniform(10, size=(self.n_genes, self.n_components))
+        # self.p /= self.p.sum(axis=1, keepdims=True)
+
+        self.z = np.arange(self.n_cells, dtype=int)
 
         # EM-algorithm
-        for _ in range(10):
-            nbm._e_step()
-            nbm._m_step()
+        self._m_step()
+        for _ in range(self.maxiter):
+            self._e_step()
+            self._m_step()
 
     def predict(self, X: np.ndarray):
-        pass
+        self.X = X
+        self._e_step()
+        return self.z
 
     def fit_predict(self, X: np.ndarray):
         self.fit(X)
-        return self.predict(self, X)
+        return self.predict(X)
     
     def _e_step(self):
         log_lh = self._log_likelihood()
-        print(log_lh)
         self.z = np.argmax(log_lh, axis=1)
-        print(self.z)    
         # this would instead yield one-hot encoding
         # z = np.zeros_like(log_lh)
         # z[np.arange(z.shape[0]), np.argmax(log_lh, axis=1)] = 1
         
-
     def _m_step(self):
         mean_p = self.p.mean(axis=1, keepdims=True)
         assigned_p = self.p[:, self.z]
@@ -66,10 +75,11 @@ class NBMixture():
     def _log_likelihood(self):
         short_p = self.p[self.S, :]
         short_X = self.X[:, self.S]
-        print(self.pi, self.r, short_p, short_X)
         return np.log(self.pi) + (short_X @ np.log(short_p) + np.sum(self.r * np.log(1-short_p), axis=0))
-
-
+    
+    def score(self, X: np.ndarray):
+        self.X = X
+        return self._log_likelihood().mean()
 
 
 if __name__ == "__main__":
@@ -77,7 +87,8 @@ if __name__ == "__main__":
     nbm = NBMixture(n_components=n_components, n_S=5)
 
     cells = np.array(['1', '2', '3', '4', '5', '6', '7', '8'])
-    S = np.array(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+    genes = np.array(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
     np.random.seed(246)
-    X = np.random.randint(0, 2, size=(cells.shape[0], S.shape[0]))
-    nbm.fit(X)
+    X = np.random.randint(0, 2, size=(cells.shape[0], genes.shape[0]))
+    print(nbm.fit_predict(X))
+    print(nbm.score(X))
--- final_project/utils.py
+++ final_project/utils.py
@@ -1,16 +1,43 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_2d_vis(results, title, cluster_colors):
-    """ Plots the first two components of the results dictionary as a grid of scatter plots."""
+# def plot_2d_vis(results, title, cluster_colors):
+#     """ Plots the first two components of the results dictionary as a grid of scatter plots."""
+#     num_rows = len(results.keys())
+#     num_cols = len(list(results.values())[0].keys())
+#     fig, axs = plt.subplots(
+#         num_rows,
+#         num_cols,
+#         figsize=(num_rows * 5, num_cols * 5),
+#     )
+#     if axs.ndim == 1:
+#         axs = axs.reshape(1, -1)
+#     fig.suptitle(title, fontsize=25)
+#     for i, (norm_key, sub_dict) in enumerate(results.items()):
+#         for j, (trans_key, result) in enumerate(sub_dict.items()):
+#             axs[i, j].scatter(
+#                 result[:, 0],
+#                 result[:, 1],
+#                 s=7,
+#                 c=cluster_colors,
+#             )
+#             axs[i, j].set_title(f"{norm_key} {trans_key}".title())
+#             axs[i, j].set_xticks([])
+#             axs[i, j].set_yticks([])
+
+
+def plot_2d_vis(results, title, clusters=None):
+    # plot t-SNE
     num_rows = len(results.keys())
     num_cols = len(list(results.values())[0].keys())
     fig, axs = plt.subplots(
         num_rows,
         num_cols,
-        figsize=(num_rows * 5, num_cols * 5),
+        figsize=(num_cols * 5, num_rows * 5),
     )
     if axs.ndim == 1:
-        axs = axs.reshape(1, -1)
+        axs = axs.reshape(-1, 1)
+
     fig.suptitle(title, fontsize=25)
     for i, (norm_key, sub_dict) in enumerate(results.items()):
         for j, (trans_key, result) in enumerate(sub_dict.items()):
@@ -18,8 +45,12 @@ def plot_2d_vis(results, title, cluster_colors):
                 result[:, 0],
                 result[:, 1],
                 s=7,
-                c=cluster_colors
+                c=clusters[norm_key][trans_key]
+                if clusters is dict
+                else clusters[norm_key][trans_key],
+                cmap="tab20" if clusters is dict else None,
             )
             axs[i, j].set_title(f"{norm_key} {trans_key}".title())
             axs[i, j].set_xticks([])
             axs[i, j].set_yticks([])
+

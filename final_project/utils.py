de00707641f440a9c287875f130d807b1c1b9120
Zleander <zimmermannleander@gmail.com>
2023-07-30 13:38:48 +0200

    update plot_2d_vis and get nbmm results

--- final_project/utils.py
+++ final_project/utils.py
@@ -1,56 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# def plot_2d_vis(results, title, cluster_colors):
-#     """ Plots the first two components of the results dictionary as a grid of scatter plots."""
-#     num_rows = len(results.keys())
-#     num_cols = len(list(results.values())[0].keys())
-#     fig, axs = plt.subplots(
-#         num_rows,
-#         num_cols,
-#         figsize=(num_rows * 5, num_cols * 5),
-#     )
-#     if axs.ndim == 1:
-#         axs = axs.reshape(1, -1)
-#     fig.suptitle(title, fontsize=25)
-#     for i, (norm_key, sub_dict) in enumerate(results.items()):
-#         for j, (trans_key, result) in enumerate(sub_dict.items()):
-#             axs[i, j].scatter(
-#                 result[:, 0],
-#                 result[:, 1],
-#                 s=7,
-#                 c=cluster_colors,
-#             )
-#             axs[i, j].set_title(f"{norm_key} {trans_key}".title())
-#             axs[i, j].set_xticks([])
-#             axs[i, j].set_yticks([])
-
-
-def plot_2d_vis(results, title, clusters=None):
+def plot_2d_vis(results, title, clusters=None, transpose=False):
     # plot t-SNE
-    num_rows = len(results.keys())
-    num_cols = len(list(results.values())[0].keys())
+    n_norms = len(results.keys())
+    n_trans = len(list(results.values())[0].keys())
+    num_rows = n_norms if not transpose else n_trans
+    num_cols = n_trans if not transpose else n_norms
     fig, axs = plt.subplots(
         num_rows,
         num_cols,
         figsize=(num_cols * 5, num_rows * 5),
     )
-    if axs.ndim == 1:
+    if axs.ndim == 1 and transpose:
+        axs = axs.reshape(1, -1)
+    elif axs.ndim == 1 and not transpose:
         axs = axs.reshape(-1, 1)
 
     fig.suptitle(title, fontsize=25)
     for i, (norm_key, sub_dict) in enumerate(results.items()):
         for j, (trans_key, result) in enumerate(sub_dict.items()):
-            axs[i, j].scatter(
+            row, col = (i, j) if not transpose else (j, i)
+            axs[row, col].scatter(
                 result[:, 0],
                 result[:, 1],
                 s=7,
                 c=clusters[norm_key][trans_key]
-                if clusters is dict
-                else clusters[norm_key][trans_key],
-                cmap="tab20" if clusters is dict else None,
+                if type(clusters) is dict
+                else clusters,
+                cmap="tab20" if type(clusters) is dict else None,
             )
-            axs[i, j].set_title(f"{norm_key} {trans_key}".title())
-            axs[i, j].set_xticks([])
-            axs[i, j].set_yticks([])
-
+            axs[row, col].set_title(f"{norm_key} {trans_key}".title())
+            axs[row, col].set_xticks([])
+            axs[row, col].set_yticks([])

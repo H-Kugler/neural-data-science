02c7dfaf63cb7bf59f3766d8ac1e2af31d225bee
H-Kugler <harald.kugler@student.uni-tuebingen.de>
2023-07-31 09:54:22 +0200

    run on leanders machine

--- final_project/utils.py
+++ final_project/utils.py
@@ -1,8 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def choose_n_clusters(
+    X: np.ndarray, mm_class, max_clusters: int = 10, min_n_clusters: int = 1
+):
+    """
+    Chooses the number of clusters using BIC.
+    returns: best_n_clusters and its corresponding predictions
+    """
+    min_bic = np.inf
+    for n_clusters in range(min_n_clusters, max_clusters + 1):
+        mm = mm_class(n_components=n_clusters)
+        mm.fit(X)
+        bic = mm.bic(X)
+        if bic < min_bic:
+            min_bic = bic
+            best_n_clusters = n_clusters
+            best_mm = mm
+    print(f"Best result: {best_n_clusters} clusters, BIC = {min_bic}")
+    return best_n_clusters, best_mm.predict(X)
+
+
 def plot_2d_vis(results, title, clusters=None, transpose=False):
-    # plot t-SNE
+    """
+    creates a 2d visualization of the results
+    """
     n_norms = len(results.keys())
     n_trans = len(list(results.values())[0].keys())
     num_rows = n_norms if not transpose else n_trans
@@ -25,11 +48,84 @@ def plot_2d_vis(results, title, clusters=None, transpose=False):
                 result[:, 0],
                 result[:, 1],
                 s=7,
-                c=clusters[norm_key][trans_key]
-                if type(clusters) is dict
-                else clusters,
+                c=clusters[norm_key][trans_key] if type(clusters) is dict else clusters,
                 cmap="tab20" if type(clusters) is dict else None,
             )
             axs[row, col].set_title(f"{norm_key} {trans_key}".title())
             axs[row, col].set_xticks([])
             axs[row, col].set_yticks([])
+
+
+class NBMM:
+    """
+    Implementation of the Negative Binomial Mixture Model
+    """
+
+    def __init__(self, r=2, n_components=2, max_iter=1000):
+        self.n_components = n_components
+        self.r = r  # paper said that this can be fixed to 2
+        self.pi = np.ones(n_components) / n_components
+        self.max_iter = max_iter
+
+        self.eps = 1e-4
+
+    def fit(self, X: np.ndarray):
+        N, G = X.shape
+        self.p = self.r / (self.r + np.random.rand(G, self.n_components))
+
+        # EM-algorithm
+        for _ in range(self.max_iter):
+            responsibilities = self._e_step(X)
+            self._m_step(X, responsibilities)
+
+        return self.p
+
+    def predict(self, X: np.ndarray):
+        return self._e_step(X).argmax(axis=1)
+
+    def fit_predict(self, X: np.ndarray):
+        self.fit(X)
+        return self.predict(X)
+
+    def _e_step(self, X: np.ndarray):
+        """
+        Expectation step of the EM algorithm
+        """
+        ll = self._log_likelihood(X)
+        maxima = np.argmax(ll, axis=1)
+        z = np.eye(self.n_components)[maxima]
+        return z
+
+    def _m_step(self, X: np.ndarray, responsibilities: np.ndarray):
+        """
+        Maximization step of the EM algorithm
+        """
+        N_k = responsibilities.sum(axis=0)
+        self.pi = (N_k + self.eps) / N_k.sum()
+        mu = (X.T @ responsibilities + 1e-4) / (N_k + 1)
+        self.p = mu / (self.r + mu)
+
+    def _log_likelihood(self, X: np.ndarray):
+        """
+        Compute the log likelihood of the data given the model current parameters
+        """
+        log_likelihood = 0
+        log_likelihood = (
+            np.log(self.pi + self.eps)
+            + X @ np.log(self.p + self.eps)
+            + np.sum(self.r * np.log(1 - self.p), axis=0)
+        )
+        return log_likelihood
+
+    def bic(self, X: np.ndarray = None):
+        """
+        Compute the BIC of the data given the model current parameters
+        """
+        N, G = X.shape
+        z = self._e_step(X)
+        N_k = z.sum(axis=0)
+
+        return np.sum(
+            (G * np.log(N_k + self.eps)) / 2
+            + np.sum(-self._log_likelihood(X) * z, axis=0)
+        )
